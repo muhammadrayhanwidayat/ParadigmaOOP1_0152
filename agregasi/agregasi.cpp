4d4922b662937696cefc1a75fb33eeb43b7f9112
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-21 11:53:46 +0700

    membuat  output ibu dan anak

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+#include "anak.h"
+#include "ibu.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    ibu* varIbu = new ibu("dini");
+    ibu* varIbu2 = new ibu("novi");
+    anak* varAnak1 = new anak("tono");
+    anak* varAnak2 = new anak("rini");
+    anak* varAnak3 = new anak("dewi");
+
+    varIbu->tambahAnak(varAnak1);
+    varIbu->tambahAnak(varAnak2);
+
 }
 
 
