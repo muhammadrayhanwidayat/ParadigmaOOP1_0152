a8c26e38abda5f4115cf886a5980ab9ce7f18fb9
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-21 11:15:28 +0700

    membuat class anak

--- agregasi/anak.h
+++ agregasi/anak.h
@@ -1,5 +1,14 @@
 #ifdef ANAK_H
 #define ANAK_H
 
-class anak{};
+class anak{
+public:
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << "anak \"" << nama << "\"ada\n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\"tidak ada\n";
+	}
+};
 #endif
