d23d888568a512f042022b9de52c1e4cdb155715
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-21 11:28:47 +0700

    membuat class ibu

--- agregasi/ibu.h
+++ agregasi/ibu.h
@@ -1,3 +1,26 @@
+
 #ifndef IBU_H
 #define IBU_H
 
+class ibu {
+public:
+	string nama;
+	vector<anak*> daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "ibu \"" << nama << "\" ada\n";
+	}
+	~ibu() {
+		cout << "ibu \"" << nama << "\" tidak ada\n";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
+};
+void ibu::cetakAnak() {
+	cout << "Daftar Anak dari Ibu \"" << this->nama << "\":\n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+#endif
