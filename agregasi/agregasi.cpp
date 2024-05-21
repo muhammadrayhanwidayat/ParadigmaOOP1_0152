abf704d5d54b900d0d4d802b3d105d4a009530d7
muhammadrayhanwidayat <murayat99@gmail.com>
2024-05-21 12:00:34 +0700

    membuat var ibu dengan fungsi dna delet ibu

--- agregasi/agregasi.cpp
+++ agregasi/agregasi.cpp
@@ -16,7 +16,15 @@ int main()
 
     varIbu->tambahAnak(varAnak1);
     varIbu->tambahAnak(varAnak2);
+    varIbu2->tambahAnak(varAnak3);
+    varIbu2->tambahAnak(varAnak1);
 
+    varIbu->cetakAnak();
+    varIbu2->cetakAnak();
+
+    delete varIbu;
+    delete varIbu2;
+    delete varAnak1;
 }
 
 
